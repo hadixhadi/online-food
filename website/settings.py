185d679db4c0816f646c2e92671651845403d089
khode hadi <hadi.hadi.oc@gmail.com>
2023-06-04 12:09:41 +0330

    change database engine from sqllite3 to postgresql

--- website/settings.py
+++ website/settings.py
@@ -71,9 +71,13 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        'PASSWORD': DB_PASSWORD,
+        'HOST': DB_HOST,
+        'PORT': DB_PORT,
     }
 }
 
